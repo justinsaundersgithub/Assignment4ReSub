2ecc8c088f25395cf384882f37a9c8d06a0cbe9b
justinsaundersgithub <44852331+justinsaundersgithub@users.noreply.github.com>
2018-12-06 19:19:10 -0800

    Add files via upload
    Correct set of files:
    Runs program correctly and with object oriented design approach
    Need to do:
    Add comments
    Run program multiple times
    Create Graphs to represent difference between BST and unsorted array

--- Product.h
+++ Product.h
@@ -1,21 +1,45 @@
 #pragma once
 
+#include <cassert>
+#include <cstdint>
+#include <ostream>
 #include <string>
 
+/**
+ */
 class Product {
+private:
+    std::uint64_t UPC;
+    std::string   description;
+
 public:
     Product(
-        std::string const& UPC,
-        std::string const& description);
-    ~Product() = default;
-    Product(Product const& other);
-    Product& operator=(Product const& rhs);
+        std::uint64_t      upc,
+        std::string const& desc)
+        : UPC(upc)
+        , description(desc) {
 
-    std::string getName() const;
+        assert(UPC != 0);
+    }
 
-private:
-    std::string UPC;
-    std::string description;
+    Product(std::uint64_t upc)
+        : Product(upc, "") {
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, Product const& item){
+        os << "|" <<"item:"<<  item.UPC << "|";
+        return os;
+    }
+
+    friend bool operator<(Product const& lhs, Product const& rhs){
+        return lhs.UPC < rhs.UPC;
+    }
+
+    friend bool operator>(Product const& lhs, Product const& rhs){
+        return lhs.UPC > rhs.UPC;
+    }
+
+    friend bool operator==(Product const& lhs, Product const& rhs){
+        return lhs.UPC == rhs.UPC;
+    }
 };
-
-
--- main.cpp
+++ main.cpp
@@ -1,54 +1,80 @@
 #include "Product.h"
-#include "Search.h"
+#include "BinarySearchTree.h"
+#include "ArraySearch.h"
+#include <cassert>
+#include <chrono>
+#include <cstdint>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 
-using namespace std;
-
-
-
-int main(){
-
-
-
-
-Search iTems;
-Product item("725177540363","Belle and The Yank 1/4");
-int e = -1;
-
-
-
-ifstream file;
-file.open("test.txt", std::ios::binary);
-if(file.is_open()){
-	
-	while(!file.eof()){
-		cout<<"File Opened"<<endl;//
-		string s1;
-		getline(file, s1, ','); 
-        string UPC = s1;
-     
-        string s2;
-        
-        cout<<UPC<<endl;
-        getline(file, s2, ',');
-
-        string description = s2;
-        cout<<description<<endl;
-        iTems.insertProductBST(item);
-        iTems.insertArray(item);
-        
-        
-    }
-    
+/**
+ * Converts a string number to an unsigned 64 bit integer.
+ * @param numberString
+ *     An unsigned integer number without any spaces, delimeters, etc.
+ * @return
+ *     The number string converted to an unsigned 64 bit integer.
+ */
+std::uint64_t stringToUInt64(std::string const& numberString) {
+    char* endOfScan = nullptr;
+    std::uint64_t result = std::strtoull(numberString.c_str(), &endOfScan, 10);
+    assert(*endOfScan == '\0'); // ...assumption is that we succeeded
+    return result;
 }
 
 
-iTems.searchProductBST(e,item);
-iTems.searchProductArray(item);
+/**
+ * Creates the product tree from the records in the input file.
+ * @param inputFilename
+ *     Input filename. The assumption is that it exists.
+ * @param productDatabase
+ *     A product tree to put the items from the file into.
+ */
+template <class DataBaseType>
+void createProductDatabase(
+    std::string const& inputFilename,
+    DataBaseType&      productDatabase) {
 
+    std::ifstream inputFile;
+    inputFile.open(inputFilename);
+    assert(inputFile.is_open()); // ...assumption is that it exists and we can open it
 
-return 0;
+    std::string upc;
+    std::string desc;
+    while (getline(inputFile, upc, ',') &&
+           getline(inputFile, desc)) {
+        Product product(stringToUInt64(upc), desc);
+        productDatabase.insert(product);
+    }
 
+    inputFile.close();
+}
 
+template <class DataBaseType>
+void testDatabase(std::string const& inputFilename) {
+    DataBaseType productDatabase;
+    createProductDatabase<DataBaseType>(inputFilename, productDatabase);
+
+    auto startTime = std::chrono::steady_clock::now();
+    bool found = productDatabase.search(Product(873454577));
+    auto endTime = std::chrono::steady_clock::now();
+
+    if (found) {
+        std::cout << "Elapsed time in milliseconds : " 
+              << std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count()
+              << " ms" << std::endl;
+    } else {
+        std::cout << "Record not found." << std::endl;
+    }
+}
+
+/**
+ */
+int main(){
+    std::string const inputFilename = "test.txt";
+    std::cout << "BST test" << std::endl;
+    testDatabase<BinarySearchTree<Product>>(inputFilename);
+    std::cout << "Array test" << std::endl;
+    testDatabase<ArraySearch<Product>>(inputFilename);
+    return 0;
 }
